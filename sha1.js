995b3b96602589d2fca116ca09c01ae1c918fabe
Hisma Mulya S <hisma@yesbossnow.com>
2016-09-19 09:22:07 +0700

    Add substring functionality

--- sha1.js
+++ sha1.js
@@ -7,8 +7,15 @@ let program = require("commander");
 
 program.version("0.5.0")
     .arguments("<string>")
-    .action((string) => {
-        console.log(sha1(string));
+    .option("-n, --number <number>", "Limit to first n number")
+    .action((string, options) => {
+        let number = options.number; 
+        if(number){
+            let substr = sha1(string).substring(0, number);
+            console.log(substr);
+        }
+        else 
+            console.log(sha1(string));
     });
 
 program.parse(process.argv);
